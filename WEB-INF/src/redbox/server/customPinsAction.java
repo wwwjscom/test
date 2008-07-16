1a5783812637cd7af10d1a5dd5b60423694fc981
Jason Soo <wwwjscom@gmail.com>
2008-07-16 08:17:33 -0500

    reverted back to old files and created copies of the updated ones with new names

--- WEB-INF/src/redbox/server/customPinsAction.java
+++ WEB-INF/src/redbox/server/customPinsAction.java
@@ -1,102 +1,108 @@
 /*
  * AdHocSubmitAction.java
  */
- 
+
 package redbox.server;
- 
+
 import java.io.*;
 import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
- 
+
 import org.apache.struts.action.*;
- 
+
 import org.jdom.*;
 import org.jdom.input.SAXBuilder;
 import org.jdom.output.XMLOutputter;
 import org.jdom.output.Format;
- 
+
 import redbox.client.pinsend.*;
- 
+
 public class AdHocSubmitAction extends Action {
- 
+
   private static PinSend pinSend   = null;
   private static SAXBuilder parser = null;
- 
+
   private String      dispatch = null; // from form
   private String      command  = null; // from action
   private HttpSession session  = null;
- 
-  private ArrayList list = null;  
-  
+  private String pin = "lawl";
+
+  private ArrayList list = null;
+
   public ActionForward execute(ActionMapping       mapping,
                                ActionForm          form,
-                               HttpServletRequest  request, 
+                               HttpServletRequest  request,
                                HttpServletResponse response) throws Exception {
- 
+
     try {
- 
+
       if (pinSend == null) {
-          
+
         String parameters[] = {"-p", "tcp", "-i", "localhost", "-n", "4444", "-r", "true"};
         pinSend = new PinSend(parameters);
       }
-      
+
       if (parser == null) {
-          
+
         parser = new SAXBuilder("org.apache.xerces.parsers.SAXParser");
-      }      
-      
+      }
+
       AdHocSubmitForm _form = (AdHocSubmitForm)form;
       dispatch = _form.getDispatch();
-      
+
       command = request.getParameter("command");
       session = request.getSession(true);
-      
+
       if (command != null) {
-        
+
         if (command.equals("initialize")) {
-          
+
           // nothing
         }
-        
+
         return(mapping.findForward("success"));
       }
       else {
-        
+
         ActionMessages messages = new ActionMessages();
-        
+
         if (dispatch.equals("submit")) {
-        
+
+          _form.setPins(request.getParameter("pins"));
           _form.setResults(submitPin(_form));
         }
-          
+
         return(mapping.findForward("success"));
-      }      
+      }
     }
     catch (Exception e) {
-      
+
       e.printStackTrace();
     }
-    
+
     return (mapping.findForward("adhocsubmit"));
   }
- 
+
   private String submitPin(AdHocSubmitForm form) throws Exception {
-      
-    pinSend.setString(form.getPin());
-    
+
+    //pinSend.setString(form.getPin());
+    pinSend.setString(form.getFormattedQuery());
+
     Vector results = pinSend.send();
- 
+
     String result  = (String)results.get(0);
-        
+
     Document document = parser.build(new StringReader(result));
     Element ele_results = document.getRootElement();
-    
-    XMLOutputter xmloutputter = new XMLOutputter(Format.getPrettyFormat());  
+
+    XMLOutputter xmloutputter = new XMLOutputter(Format.getPrettyFormat());
     String xmlout = xmloutputter.outputString(ele_results);
-    
-    return(xmlout);
+
+    //return(form.getFormattedQuery());
+    //return(form.getPin());
+    //return(xmlout);
+    return("Lawl");
   }
-  
+
 }
---
+++ WEB-INF/src/redbox/server/customPinsForm.java
@@ -0,0 +1,136 @@
+/*
+ * AdHocSubmitForm.java
+ */
+
+package redbox.server;
+
+import org.apache.struts.action.*;
+
+public class AdHocSubmitForm extends ActionForm {
+
+  private String dispatch = null;
+  private String pin      = null;
+  private String pins     = null;//DEBUG
+  private String results  = null;
+
+  public void setDispatch(String dispatch) {
+
+    this.dispatch = dispatch;
+  }
+
+  public String getDispatch() {
+
+    return(dispatch);
+  }
+
+  public void setPin(String pin) {
+     this.pin = pin;
+  }
+
+  public String getPin() {
+
+    return(pin);
+  }
+
+  public void setResults(String results) {
+
+    this.results = results;
+  }
+
+  public String getResults() {
+
+    return(results);
+  }
+  public ActionErrors validate(ActionMapping mapping, javax.servlet.http.HttpServletRequest request) {
+
+    ActionErrors errors = new ActionErrors();
+
+    String command = request.getParameter("command");
+
+    if ((command != null) && (command.equals("initialize"))) {
+
+      reset();
+
+      return(errors);
+    }
+    else {
+
+      // check for null pin value
+    }
+
+    return(errors);
+  }
+
+  public void reset() {
+
+    setDispatch(null);
+    setPin(null);
+    setResults(null);
+  }
+
+  /* <Added by soo@IIT> */
+  public String getFormattedQuery()
+  {
+          String q = null;
+          if(pins.equals("redbox"))
+          {
+
+                  q = "<PIN>"
+                            + "<HDT>"
+                                + "<PINname>REDBOX_Administration</PINname>"
+                                    + "<PINtype>redboxStatus</PINtype>"
+                              + "</HDT>"
++ getPin()
+                      + "</PIN>";
+
+          } else if (pins.equals("listrules")) {
+
+                q = "listrules";
+
+          } else if (pins.equals("sql")) {
+
+                q = getPin();
+
+          } else if (pins.equals("xmlql")) {
+
+                q = "<PIN>"
+                + "<HDT Version=\"1.0\">"
+		+ "<PINname>Query</PINname>"
+                + "<PINtype>XMLQL</PINtype>"
+                + "</HDT>"
+                + "<QDT>"
+                        + "<Query description='TV Transmitter Call Signs as XML'"
+                        + "explain='false'"
+                        + "gcausage='false'"
+                        + "language='xmlql'"
+                        + "joinType='1'"
+                        + "numcols='1'"
+                        + "tlevel='4'"
+                        + "mimetype='text/xml'"
+                        + "preservewhitespace='true'"
+                        + "cdata='false' >"
+                + "<![CDATA[ "
+                + getPin()
+                + "]]>"
+                + "</Query>"
+                + "</QDT></PIN>";
+
+          } else if (pins.equals("xpath")) {
+
+                q = "<PIN>  <HDT Version=\"1.0\">    <PINname>Query</PINname>    <PINtype>XMLQL</PINtype>  </HDT>  <QDT>    <Query language=\"xpath\" gcausage=\"false\"         >      <![CDATA[ " + getPin() + "      ]]>    </Query>  </QDT></PIN>";
+          }
+
+          return q;
+  }
+
+  public void setPins(String pins)
+  {
+          this.pins = pins;
+  }
+
+  public String getPins()
+  {
+          return pins;
+  }
+  /* </Added by soo@IIT> */
+}
